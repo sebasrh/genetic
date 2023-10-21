e0af40d4dc5c8a06a2e0f84d89394e7b9c04da11
sebasrh <sebastian.hidalgo@correounivalle.edu.co>
2023-10-20 20:45:02 -0500

    Listo para desplegar

--- geneticalgorithm/settings.py
+++ geneticalgorithm/settings.py
@@ -128,12 +128,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGIN_URL = "/accounts/signin"
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
