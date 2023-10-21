7df2ed8fac2bb19881dd0210ee71ffb094bb26ff
sebasrh <sebastian.hidalgo@correounivalle.edu.co>
2023-10-20 23:36:07 -0500

    Listo para desplegar

--- algorithm/midi_utils.py
+++ algorithm/midi_utils.py
@@ -1,14 +1,23 @@
-import subprocess
+import  fluidsynth
 from pydub import AudioSegment
+import os
 
 def convert_midi_to_wav_mp3(file_midi, soundfont_path):
     # Build file paths
     file_wav = file_midi.replace(".mid", ".wav")
     file_mp3 = file_midi.replace(".mid", ".mp3")
 
-    # Use FluidSynth to convert MIDI to WAV
-    fluidsynth_command = f"fluidsynth -a alsa -g 1.0 -l -i '{soundfont_path}' '{file_midi}' -F '{file_wav}'"
-    subprocess.run(fluidsynth_command, shell=True)
+    fs = fluidsynth.Synth() # Create synthesizer instance (plays audio) 
+    fs.start(driver="dsound") # Start audio driver (For windows, use driver="dsound")
+    sfid = fs.sfload(soundfont_path) # Load SoundFont file 
+    fs.program_select(0, sfid, 0, 0) # Select program for a channel 
+
+    fs.midi_file_load(file_midi) # Load MIDI file 
+
+    audio_data = fs.get_samples() # Get samples as NumPy array 
+
+    with open(file_wav, "wb") as f: # Save as WAV file 
+        f.write(bytes(audio_data)) 
 
     # Load the WAV file using pydub
     audio = AudioSegment.from_wav(file_wav)
@@ -24,5 +33,9 @@ def convert_midi_to_wav_mp3(file_midi, soundfont_path):
 
     # Calculate and store the final duration
     final_duration = len(adjusted_audio) / 1000.0  # Convert milliseconds to seconds
+
+    os.remove(file_wav) # Delete the WAV file (not needed anymore) 
+
+    fs.delete() # Delete synthesizer instance 
 
     return file_wav, file_mp3, final_duration
